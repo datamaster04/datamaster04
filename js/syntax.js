ca97125b3fb2cfb3c928a65239b542138b612551
Sondre <datamaster04@gmail.com>
2021-12-09 13:31:44 +0100

    Update syntax.js

--- js/syntax.js
+++ js/syntax.js
@@ -1,7 +1,7 @@
 const dateNumber = date => date.getFullYear() + (date.getMonth() + 1) / 12 + date.getDay() / 365.25;
 
 const now = new Date(Date.now())
-const birthDate = new Date('2004/06/13 04:25 PM');
+const birthDate = new Date('2004/06/13');
 
 const age = Math.floor(dateNumber(now) - dateNumber(birthDate));
 
