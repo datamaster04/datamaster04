4439fbbc03893ce85215c903b1e58c2a798a68c8
Sondre <datamaster04@gmail.com>
2021-12-09 13:29:31 +0100

    date

--- js/syntax.js
+++ js/syntax.js
@@ -1,8 +1,10 @@
 const dateNumber = date => date.getFullYear() + (date.getMonth() + 1) / 12 + date.getDay() / 365.25;
 
 const now = new Date(Date.now())
-const birthDate = new Date('2003/06/13');
+const birthDate = new Date('2004/06/13 04:25 PM');
 
 const age = Math.floor(dateNumber(now) - dateNumber(birthDate));
 
-document.getElementById("age").InnerText = age;
+document.getElementById("age").innerText = "Alder: " + age;
+
+console.log(age)
