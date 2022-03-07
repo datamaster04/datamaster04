63b1b403bc8d9891a9d60bb31eb96a0911ebba60
datamaster04 <datamaster04@gmail.com>
2022-03-07 09:18:00 +0100

    pages

--- portfolioside/src/App.js
+++ portfolioside/src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 import './App.css';
-import * as Pages from ".";
+import Home from './pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle'
 import Header from "./components/Header";
 
 function App() {
-  return <BrowserRouter>
+  return (<BrowserRouter>
   <Header />
   <Routes>
-    <Route path="/" element={<Pages.Home />} />
+    <Route path="/" element={<Home />} />
 
-    <Route path="/om-meg" element={<Pages.OmMeg />} />
+    {/* <Route path="/om-meg" element={<Pages.OmMeg />} />
     <Route path="/cv" element={<Pages.Cv />} />
     <Route path="/prosjekter" element={<Pages.Prosjekter />} />
     <Route path="/nettbutikk" element={<Pages.Nettbutikk />} />
-    <Route path="*" element={<Navigate to="/Home" />} />
+    <Route path="*" element={<Navigate to="/Home" />} /> */}
   </Routes>
-  </BrowserRouter>
+  </BrowserRouter>)
                                                                                                                                                                                                                         
 }
 
--- portfolioside/src/index.js
+++ portfolioside/src/index.js
@@ -1,14 +1,23 @@
-import './index.css';
-import Home from "./pages/Home"
-import OmMeg from "./pages/OmMeg"
-import Cv from "./pages/Cv"
-import Prosjekter from "./pages/Prosjekter"
-import Nettbutikk from "./pages/Nettbutikk"
+// import './index.css';
+// import Home from "./pages/Home"
+// import OmMeg from "./pages/OmMeg"
+// import Cv from "./pages/Cv"
+// import Prosjekter from "./pages/Prosjekter"
+// import Nettbutikk from "./pages/Nettbutikk"
 
-export {
-  Home,
-  OmMeg,
-  Cv,
-  Prosjekter,
-  Nettbutikk
-}
+// export {
+//   Home,
+//   OmMeg,
+//   Cv,
+//   Prosjekter,
+//   Nettbutikk
+// }
+
+import React from "react";
+import ReactDOM from "react";
+import App from './App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
